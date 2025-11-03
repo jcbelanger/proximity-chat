fbb78bae6a34a396a51b99b0b88c57547bfbb3f3
Joshua Belanger <jbelanger@penbaytechgroup.com>
2025-11-03 00:08:11 -0500

    rename

--- src/main/java/com/github/jcbelanger/proximity/ProximityChatPlugin.java
+++ src/main/java/com/github/jcbelanger/proximity/ProximityChatPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.github.jcbelanger.proximity;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class ProximityChatPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private ProximityChatConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	ProximityChatConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(ProximityChatConfig.class);
 	}
 }
